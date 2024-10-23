8c8acbe9db2f1d6994f3d3e7a058376dc9525966
Phat Nguyen <tanphat199@gmail.com>
2024-10-23 11:49:31 +0700

    change color name

--- lua/lualine/themes/devglow.lua
+++ lua/lualine/themes/devglow.lua
@@ -1,36 +1,35 @@
 local colors = {
 	color0 = "#EEE2DE",
-	color1 = "#BBBBBB",
-	color4 = "#30302C",
-	color5 = "#E5B567",
-	color6 = "#444444",
-	color7 = "#797979",
-	color10 = "#87AF87",
-	color13 = "#AF5F5F",
-	color14 = "#7EAAC7",
+	color1 = "#222222",
+	color2 = "#111111",
+	color3 = "#E5B567",
+	color4 = "#797979",
+	color5 = "#87AF87",
+	color6 = "#AF5F5F",
+	color7 = "#7EAAC7",
 }
 
 return {
 	visual = {
 		b = { fg = colors.color0, bg = colors.color1 },
-		a = { fg = colors.color4, bg = colors.color5, gui = "bold" },
+		a = { fg = colors.color2, bg = colors.color3, gui = "bold" },
 	},
 	inactive = {
-		b = { fg = colors.color6, bg = colors.color4 },
-		c = { fg = colors.color6, bg = colors.color4 },
-		a = { fg = colors.color7, bg = colors.color4, gui = "bold" },
+		b = { fg = colors.color4, bg = colors.color2 },
+		c = { fg = colors.color4, bg = colors.color2 },
+		a = { fg = colors.color4, bg = colors.color2, gui = "bold" },
 	},
 	insert = {
 		b = { fg = colors.color0, bg = colors.color1 },
-		a = { fg = colors.color4, bg = colors.color10, gui = "bold" },
+		a = { fg = colors.color2, bg = colors.color5, gui = "bold" },
 	},
 	replace = {
 		b = { fg = colors.color0, bg = colors.color1 },
-		a = { fg = colors.color4, bg = colors.color13, gui = "bold" },
+		a = { fg = colors.color2, bg = colors.color6, gui = "bold" },
 	},
 	normal = {
 		b = { fg = colors.color0, bg = colors.color1 },
-		c = { fg = colors.color7, bg = colors.color4 },
-		a = { fg = colors.color4, bg = colors.color14, gui = "bold" },
+		c = { fg = colors.color4, bg = colors.color2 },
+		a = { fg = colors.color2, bg = colors.color7, gui = "bold" },
 	},
 }
